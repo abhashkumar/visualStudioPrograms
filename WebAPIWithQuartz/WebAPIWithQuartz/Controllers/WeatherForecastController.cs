d0511bc8a5add01536557fb2570e43924a44c00f
abhashkumar <abhashmaddi@hotmail>
2024-05-16 21:58:18 +0530

    Code refactoring

--- WebAPIWithQuartz/WebAPIWithQuartz/Controllers/WeatherForecastController.cs
+++ WebAPIWithQuartz/WebAPIWithQuartz/Controllers/WeatherForecastController.cs
@@ -11,18 +11,6 @@ namespace WebAPIWithQuartz.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet]
         public async Task Get()
         {
